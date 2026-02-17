0b843d18fd72242d86653dbfa6ec149a8f010bce
aura7822 <joshuaura7822@gmail.com>
2026-02-17 11:39:14 +0300

    Initial commit: Student Management System

---
+++ StudentApplication/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApplication.Models;
+
+namespace StudentApplication.Data
+{
+    public class ApplicationDbContext : DbContext  // Fixed: Changed from ApplicationDBContext to ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentApplication/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Course { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+    }
+}
